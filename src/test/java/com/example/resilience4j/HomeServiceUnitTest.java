125fa4b3d1d3c747763e74d6b0739e9ffec797dd
Smit <smitshah95@hotmail.com>
2020-03-12 02:36:13 +0800

    fixed #1 by adding SpringBootTest annotation and acuator dependency

--- src/test/java/com/example/resilience4j/HomeServiceUnitTest.java
+++ src/test/java/com/example/resilience4j/HomeServiceUnitTest.java
@@ -9,12 +9,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class HomeServiceUnitTest extends AbstractCircuitBreakerTest {
 
     @Autowired
