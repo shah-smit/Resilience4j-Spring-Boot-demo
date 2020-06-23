0eac1eb3fc26a01b26ab113e6c4326def548fce3
Smit <smitshah95@hotmail.com>
2020-06-23 23:06:09 +0800

    upgraded pom.xml versions

--- src/test/java/com/example/resilience4j/Service/HomeServiceUnitTest.java
+++ src/test/java/com/example/resilience4j/Service/HomeServiceUnitTest.java
@@ -5,17 +5,17 @@ import com.example.resilience4j.Controller.FailureService;
 import com.example.resilience4j.Controller.HomeService;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.vavr.collection.Stream;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.mockito.Mockito.when;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @DirtiesContext
 public class HomeServiceUnitTest extends AbstractCircuitBreakerTest {
