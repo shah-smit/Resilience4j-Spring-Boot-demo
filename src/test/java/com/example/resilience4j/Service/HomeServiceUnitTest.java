82c71a6d58f6b2ac9b6502caaed7beb8e73f29e9
Smit <smitshah95@hotmail.com>
2020-06-24 02:16:19 +0800

    added test cases to test config

--- src/test/java/com/example/resilience4j/Service/HomeServiceUnitTest.java
+++ src/test/java/com/example/resilience4j/Service/HomeServiceUnitTest.java
@@ -10,14 +10,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.mockito.Mockito.when;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@DirtiesContext
+@ExtendWith(SpringExtension.class)
 public class HomeServiceUnitTest extends AbstractCircuitBreakerTest {
 
     @Autowired
@@ -27,11 +25,38 @@ public class HomeServiceUnitTest extends AbstractCircuitBreakerTest {
     FailureService failureService;
 
     @Test
-    public void WhenFailureServiceThrowsErrorTenTimesThenCircuitBreakerStateShouldChangeToOPEN(){
+    public void WhenFailureServiceThrowsErrorTenTimesThenCircuitBreakerStateShouldChangeToOPEN() throws InterruptedException {
         when(failureService.getGreeting()).thenThrow(new RuntimeException());
 
         Stream.rangeClosed(1,10).forEach((count) -> service.getGreeting());
 
         checkHealthStatus(CB_INSTANCE_FailureService, CircuitBreaker.State.OPEN);
+
+        Thread.sleep(5000);
+
+        checkHealthStatus(CB_INSTANCE_FailureService, CircuitBreaker.State.HALF_OPEN);
+
+        Stream.rangeClosed(1,10).forEach((count) -> service.getGreeting());
+
+        checkHealthStatus(CB_INSTANCE_FailureService, CircuitBreaker.State.OPEN);
+    }
+
+    @Test
+    public void whenStateGoesToHalfOpenAndSuccessFulStateThenStateBecomesClosed() throws InterruptedException {
+        //given
+        transitionToOpenState(CB_INSTANCE_FailureService);
+        transitionToHalf_OpenState(CB_INSTANCE_FailureService);
+
+        //testing whether the given set correct state
+        checkHealthStatus(CB_INSTANCE_FailureService, CircuitBreaker.State.HALF_OPEN);
+
+        //given
+        when(failureService.getGreeting()).thenReturn("Success");
+
+        //when
+        Stream.rangeClosed(1,10).forEach((count) -> service.getGreeting());
+
+        //then
+        checkHealthStatus(CB_INSTANCE_FailureService, CircuitBreaker.State.CLOSED);
     }
 }
